3fe3e183d671f88714dca8e563c881dbee88b96d
Peter Chigozirim <peterchigozirim@gmail.com>
2024-06-11 00:13:55 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function (Request $request) {
+    return ['IP Address' => $request->ip()];
+});
+
+require __DIR__.'/auth.php';
